ee96db7fa4202688e1ba25164742e37f6b018a86
Darío A. Salcidos Álvarez <92925997+DarioSalcidos@users.noreply.github.com>
2021-12-10 13:53:44 +0100

    Add files via upload

--- JavaScript/tarefa3_mates/escripito.js
+++ JavaScript/tarefa3_mates/escripito.js
@@ -16,6 +16,12 @@ var primero = Math.sqrt(aaa);
 var bbb =23.4687 ;
 var segundo=Math.pow(bbb, 2);
 	alert(segundo); 
-var ccc= 3.14159265359;
+var ccc= Math.PI;
 var tercero=Math.trunc(ccc);
-	alert(tercero);
+	alert('Número PI sin decimales=' + tercero);
+var ddd= (x=500 , y=100)
+var cuarto=Math.min(x, y)
+	alert(cuarto)
+var eee= (0-3)
+var quinto= Math.random(eee);
+	alert(quinto);
